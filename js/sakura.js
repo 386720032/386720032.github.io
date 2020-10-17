19026c5010a494f4341b0595bffbe1f6f64784c3
386720032 <386720032@qq.com>
2020-10-17 15:56:45 +0800

    Site updated: 2020-10-17 15:56:41

---
+++ js/sakura.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+//ֻ���������ҳ������Ч
+var windowWidth = $(window).width();
+if (windowWidth > 768) {
+    document.write('<script type="text/javascript" src="/js/sakura.js"><\/script>');
+}
+</script>
+
